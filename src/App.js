6aa902413812237489db48d3728ed6a84deb5370
arunreddy9966 <reddyarunkumar61@gmail.com>
2024-06-02 13:54:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react java script</h1>
+      <h2>this is my second react project</h2>
+      
+    </div>
+  );
+}
+
+export default App;
